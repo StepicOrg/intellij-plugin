17dfc38813f58bf6aca4a94d6187f4b2965e873d
2Pit <afnm47@gmail.com>
2016-04-06 14:26:02 +0300

    add idea files

---
+++ src/main/actions/ClientInfo.java
@@ -0,0 +1,15 @@
+package main.actions;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * Created by Petr on 02.04.2016.
+ */
+public class ClientInfo extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
