a337c0ebf5705d2bb9d4530c3199f93fe1df2c3d
2Pit <afnm47@gmail.com>
2016-05-27 23:42:28 +0300

    one icons problem fixed

--- src/main/icons/PluginIcons.java
+++ src/main/icons/PluginIcons.java
@@ -11,7 +11,7 @@ import javax.swing.*;
 //public class PluginIcons {
 public interface PluginIcons {
 //    public static final Icon STEPIC_LOGO = IconLoader.getIcon("/icons/stepic_logo_green.png");
-    Icon STEPIC_LOGO = IconLoader.getIcon("/icons/stepic_logo_green.png");
+    public static Icon STEPIC_LOGO = IconLoader.getIcon("/icons/stepic_logo_green.png");
 //    public static final Icon Question = AllIcons.General.TodoQuestion;
     Icon Question = AllIcons.General.TodoQuestion;
 }
