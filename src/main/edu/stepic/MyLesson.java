74e7841c3bea6aa83d7b2cad3cd3b937dc7a2c37
2Pit <afnm47@gmail.com>
2016-06-14 18:20:56 +0300

    upgrade builder/updater

--- src/main/edu/stepic/MyLesson.java
+++ src/main/edu/stepic/MyLesson.java
@@ -3,7 +3,6 @@ package main.edu.stepic;
 import com.google.gson.annotations.SerializedName;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
-import main.projectWizard.MyFileInfoList;
 import main.stepicConnector.NewProjectService;
 import main.stepicConnector.StepicConnector;
 
@@ -11,9 +10,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Created by Petr on 02.05.2016.
- */
 public class MyLesson {
 
     private static final Logger LOG = Logger.getInstance(MyLesson.class);
@@ -38,10 +34,8 @@ public class MyLesson {
                 steps.put(step.position, step);
                 String filename = "Step" + step.position;
                 String path = getPath(courseDir, sectionDir, lessonName) + filename + ".java";
-                MyFileInfoList.getInstance().addFileInfo(path, courseDir, sectionDir + "." + lessonName, filename);
-//                projectService.addPathStep(path, Integer.toString(step.id));
-//                projectService.addPathPackage(path, sectionDir + "." + lessonName);
-                projectService.addStepInfo(path, new StepInfo(Integer.toString(step.id), sectionDir + "." + lessonName ));
+                StepInfo tmp = new StepInfo(Integer.toString(step.id), sectionDir + "." + lessonName, filename);
+                projectService.addStepInfo(path, tmp);
             }
         }
     }
